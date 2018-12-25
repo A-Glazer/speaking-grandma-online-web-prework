2af5e32d06d6109e8faf09d30ab3746c0438c4f3
A Glazer <avigayilglazer@gmail.com>
2018-12-25 10:11:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,6 +21,9 @@ def speak_to_grandma(phrase)
     elsif phrase.upcase
     puts "NO, NOT SINCE 1938!"
     
+    elsif phrase.downcase
+    puts "HUH?! SPEAK UP SONNY!"
+    
   else 
     puts "HUH?! SPEAK UP SONNY!"
 end
