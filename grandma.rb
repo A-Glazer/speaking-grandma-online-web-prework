f5bf91260b4eb9922ca110bea1ab194ad53d6696
A Glazer <avigayilglazer@gmail.com>
2018-12-25 10:28:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA" && phrase.upcase
-     "I LOVE YOU TOO PUMPKIN" 
+  if phrase == "I LOVE YOU GRANDMA!" && phrase.upcase
+     "I LOVE YOU TOO PUMPKIN!" 
     
-    elsif phrase == phrase.upcase
+    elsif phrase == phrase.upcase 
      "NO, NOT SINCE 1938!"
     
     elsif phrase == phrase.downcase
